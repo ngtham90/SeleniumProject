da0d7616c1e18770d6752ec46110557b10467305
ngtham90 <tham.0909@gmail.com>
2017-10-16 14:09:02 +0700

    Update AjaxFormDemo.java

--- src/test/java/pages/AjaxFormDemo.java
+++ src/test/java/pages/AjaxFormDemo.java
@@ -1,6 +1,5 @@
 package pages;
 
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,9 +11,9 @@ public class AjaxFormDemo {
 
 	WebDriver driver;
 	WebDriverWait wait;
-	By name = By.id("title");
-	By comment = By.id("description");
-	By button = By.id("btn-submit");
+	final private static By name = By.id("title");
+	final private static By comment = By.id("description");
+	final private static By button = By.id("btn-submit");
 	By result = By.xpath(".//*[@id='submit-control']");
 	
 	public AjaxFormDemo(WebDriver driver) {
@@ -22,47 +21,50 @@ public class AjaxFormDemo {
 	}
 	
 	//Set Name
-	public void set_Name(String var1) {
+	public void setName(String var1) {
 		driver.findElement(name).sendKeys(var1);
 	}
 	
 	//Set Comment
-	public void set_Comment(String var2) {
+	public void setComment(String var2) {
 		driver.findElement(comment).sendKeys(var2);
 	}
 	
 	//Click Submit button
-	public void click_Button() {
+	public void clickOnSubmitButton() {
 		driver.findElement(button).click();
 	}
 	
-	//Verify Ajax message
-	public void try_Ajax() {
-		this.set_Name("Tham");
-		this.set_Comment("123");
-		this.click_Button();
-		WebElement ajax_result = driver.findElement(result);
-		wait = new WebDriverWait(driver, 5);
-		wait.until(ExpectedConditions.visibilityOf(ajax_result));
-		
-		String getResult = ajax_result.getText();
-	
-		Assert.assertTrue(getResult.toLowerCase().contains("ajax request is processing!"));
-		
+	//Test Ajax
+	public void tryAjax() {
+		this.setName("Tham");
+		this.setComment("123");
+		this.clickOnSubmitButton();		
 	}
 	
-	//Verify message after submission
-	public void get_text() {
+	//Get Ajax message
+	public String getAjaxMessage() {
+		
+		WebElement ajax_result = driver.findElement(result);
+		wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.visibilityOf(ajax_result));	
+		String getResult = ajax_result.getText();
+		return getResult;
+	}
+	
+	//Get message after submission
+	public String getTextAfterSubmission() {
 		
 		WebElement divEle = driver.findElement(By.id("submit-control"));
 		//Find child Node belong to div element
 		int childCount = divEle.findElements(By.tagName("input")).size();
-		if(childCount < 1){
+		if(childCount < 1){		
 			
-			Assert.assertTrue(divEle.getText().toLowerCase().contains("form submited successfully!"));
+			return divEle.getText();
 			
 		}
+		
+		return null;
 	}
 	
 }
-
