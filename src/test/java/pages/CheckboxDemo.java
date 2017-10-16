d43a1c479b515a24b7931cd0d310b5bdcc14d9ef
ngtham90 <tham.0909@gmail.com>
2017-10-16 14:09:42 +0700

    Update CheckboxDemo.java

--- src/test/java/pages/CheckboxDemo.java
+++ src/test/java/pages/CheckboxDemo.java
@@ -9,25 +9,25 @@ import org.openqa.selenium.WebElement;
 public class CheckboxDemo {
 
 	WebDriver driver;
-	By checkAll_Button = By.id("check1");
-	By option = By.className("cb1-element");
+	final private static By checkAll_Button = By.id("check1");
+	final private static By option = By.className("cb1-element");
 
 	public CheckboxDemo(WebDriver driver) {
 		this.driver = driver;
 	}
 	
 	//Click 'Check All' button
-	public void click_Check_All_Button() {
+	public void clickCheckAllButton() {
 		driver.findElement(checkAll_Button).click();
 	}
 	
 	//Get name of button after clicking
-	public String get_Button_Name() {
+	public String getButtonName() {
 		return driver.findElement(checkAll_Button).getAttribute("value");
 	}
 	
 	//Uncheck Option 2
-	public void uncheck_Option2() {
+	public void uncheckOption2() {
 	
 		List<WebElement> list = driver.findElements(option);
 		
