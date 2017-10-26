b40472fb77a788da5e263255f7a82d7f323c5f16
ngtham90 <tham.0909@gmail.com>
2017-10-26 14:52:16 +0700

    Update Navigator.java

--- src/test/java/pages/Navigator.java
+++ src/test/java/pages/Navigator.java
@@ -10,10 +10,36 @@ public class Navigator {
 		this.driver = driver;
 	}
 	
-	public void gotoPage(String a) {
-		driver.get(a);
-	}
 	
+	public void goToScreen(String a) {
+		switch(a) {
+		case "AjaxFormDemo":
+			driver.get("http://www.seleniumeasy.com/test/ajax-form-submit-demo.html");
+			break;
+		case "CheckboxDemo":
+			driver.get("http://www.seleniumeasy.com/test/basic-checkbox-demo.html");
+			break;
+		case "InputFormSubmitDemo" :
+			driver.get("http://www.seleniumeasy.com/test/input-form-demo.html");
+			break;
+		case "JqueryDemo" :
+			driver.get("http://www.seleniumeasy.com/test/jquery-dropdown-search-demo.html");
+			break;
+		case "RadioButtonsDemo":
+			driver.get("http://www.seleniumeasy.com/test/basic-radiobutton-demo.html");
+			break;
+		case "SelectDropdownListDemo":
+			driver.get("http://www.seleniumeasy.com/test/basic-select-dropdown-demo.html");
+			break;
+		case "SimpleFormDemo" :
+			driver.get("http://www.seleniumeasy.com/test/basic-first-form-demo.html");
+			break;
+		default:
+			System.out.println("Back to Homepage!");
+			driver.get("http://www.seleniumeasy.com/");
+			break;
+		}
+	}
 	
 	
 }
