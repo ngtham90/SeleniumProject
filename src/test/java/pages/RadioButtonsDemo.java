ff6691c31b097571cca3972ad2f352e8479f3a17
ngtham90 <tham.0909@gmail.com>
2017-10-26 11:07:20 +0700

    Update RadioButtonsDemo.java

--- src/test/java/pages/RadioButtonsDemo.java
+++ src/test/java/pages/RadioButtonsDemo.java
@@ -1,15 +1,17 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+
 
 public class RadioButtonsDemo {
 
 	WebDriver driver;
-	By sex = By.xpath(".//*[@value='Female'][@name='gender']");
-	By age = By.xpath(".//*[@value='5 - 15']");
-	By button = By.xpath(".//*[@class='btn btn-default'][text()='Get values']");
-	By result = By.className("groupradiobutton");
+	final private static By sex = By.xpath(".//*[@value='Female'][@name='gender']");
+	final private static By age = By.xpath(".//*[@value='5 - 15']");
+	final private static By button = By.xpath(".//*[@class='btn btn-default'][text()='Get values']");
+	final private static By result = By.className("groupradiobutton");
 	
 	public RadioButtonsDemo(WebDriver driver) {
 		this.driver = driver;
@@ -17,30 +19,44 @@ public class RadioButtonsDemo {
 	
 	//Select Sex
 	public void setSex() {
-		driver.findElement(sex).click();
+		WebElement bSex = driver.findElement(sex);
+		boolean bValue = bSex.isSelected();
+		if(bValue == true) {
+			System.out.println(" Sex is selected!");
+		}else {
+			bSex.click();
+		}
+		
 	}
 	
+	
 	//Select Age Group
-	public void set_Age_Group() {
-		driver.findElement(age).click();
+	public void setAgeGroup() {
+		WebElement bAge = driver.findElement(age);
+		boolean bValue = bAge.isSelected();
+		if(bValue == true) {
+			System.out.println("Age Group is selected!");
+		} else {
+			bAge.click();
+		}
 	}
 	
 	//Click "Get Values" button
-	public void click_Button() {
+	public void clickOnGetValuesButton() {
 		driver.findElement(button).click();
 	}
 	
 	//Get result
-	public String get_Result() {
+	public String getResult() {
 		return driver.findElement(result).getText().replaceAll("\\r\\n|\\r|\\n", " ");
 	}
 	
-	public void try_Radio_Buttons() {
+	public void tryRadioButtons() {
 		
 		this.setSex();
-		this.set_Age_Group();
-		this.click_Button();
-		this.get_Result();
+		this.setAgeGroup();
+		this.clickOnGetValuesButton();
+		this.getResult();
 	
 	}
 }
