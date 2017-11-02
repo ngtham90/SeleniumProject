e2019e731d4e8eabb2231a3de8f18a96b6a50f91
ngtham90 <tham.0909@gmail.com>
2017-11-02 11:20:52 +0700

    Update Navigator.java

--- src/test/java/pages/Navigator.java
+++ src/test/java/pages/Navigator.java
@@ -1,15 +1,12 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
 
-public class Navigator {
 
-	WebDriver driver;
+public class Navigator extends MyPage {
+
+
 	
-	public  Navigator(WebDriver driver) {
-		this.driver = driver;
-	}
-	
+
 	
 	public void goToScreen(String a) {
 		switch(a) {
