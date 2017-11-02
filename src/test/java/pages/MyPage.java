ea736c9354c58eaed1c1fd0eab3d97103be0b3aa
ngtham90 <tham.0909@gmail.com>
2017-11-02 10:28:03 +0700

    Add files via upload

---
+++ src/test/java/pages/MyPage.java
@@ -0,0 +1,13 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MyPage {
+
+	WebDriver driver;
+	WebDriverWait wait;
+	
+	}
+
+
